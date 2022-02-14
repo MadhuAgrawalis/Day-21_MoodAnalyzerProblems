e41a17905944d599060a392d5c5448407ac5dc1e
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-14 13:13:39 +0530

    UC1

---
+++ Day-21_UserRegistrationAndMoodAnalyzerProblems/MoodAnalyser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestProject2
+{
+    internal class MoodAnalyser
+    {
+        private string empty;
+
+        public MoodAnalyser()
+        {
+        }
+
+        public MoodAnalyser(string empty)
+        {
+            this.empty = empty;
+        }
+
+        internal void AnalyzeMood()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
